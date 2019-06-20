64273e0387d2624c8109a384ff7f46d720eb5ef2
kod3180 <kod3180@gmail.com>
2019-06-20 12:18:03 +0000

    header interactivity added

--- app/assets/scripts/index.js
+++ app/assets/scripts/index.js
@@ -1,10 +1,12 @@
 import "../scss/style.scss"
 import Modal from "./modules/modal"
+import Header from "./modules/header"
 
 window.onload = (() => {
   'use strict'
 
-  new Modal()
+  new Modal();
+  new Header();
 
 
 })
---
+++ app/assets/scripts/modules/header.js
@@ -0,0 +1,68 @@
+import $ from "jquery";
+import "waypoints/lib/noframework.waypoints.min";
+import smoothScroll from "jquery-smooth-scroll";
+
+class Header {
+  constructor() {
+    this.pageHeader = $(".page-nav");
+    this.triggerElement = $(".hero-large__heading");
+    this.pageSections = $(".page-section");
+    this.links = $(".link");
+    this.showHeader = this.showHeader.bind(this);
+    this.showHeader();
+    this.addsmoothScrolling();
+    this.currentSection();
+  }
+
+  showHeader() {
+    let changeClass = direction => {
+      return direction === "down"
+        ? this.pageHeader.addClass("page-nav__isdarker")
+        : this.pageHeader.removeClass("page-nav__isdarker");
+    };
+
+    new Waypoint({
+      element: this.triggerElement[0],
+      handler: function(direction) {
+        return changeClass(direction);
+      }
+    });
+  }
+
+  addsmoothScrolling() {
+    this.links.smoothScroll();
+  }
+
+  currentSection() {
+    let self = this;
+    this.pageSections.each(function () {
+      let el = this;
+        new Waypoint({
+            element: el,
+            handler: (direction) => {
+                if (direction === "down") {
+                    let matchedLink = el.getAttribute('data-link');
+                    self.links.removeClass("isCurrentActive");
+                    $(matchedLink).addClass("isCurrentActive");
+                }
+            },
+            offset: "18%"
+        });
+
+        new Waypoint({
+            element: el,
+            handler: (direction) => {
+                if (direction === "up") {
+                    let matchedLink = el.getAttribute('data-link');
+                    self.links.removeClass("isCurrentActive");
+                    $(matchedLink).addClass("isCurrentActive");
+                }
+            },
+            offset: "-40%"
+        });
+    });
+  }
+}
+
+export default Header
+
