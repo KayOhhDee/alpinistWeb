fa166dabc1c24cd0e882be31df835defcabe4050
kod3180 <kod3180@gmail.com>
2019-07-15 23:16:03 +0000

    few modification made

--- app/assets/scripts/index.js
+++ app/assets/scripts/index.js
@@ -1,6 +1,6 @@
 import $ from "jquery";
 import "../scss/style.scss"
-import Modal from "./modules/modal"
+
 import Header from "./modules/header"
 import MobileMenu from "./modules/mobile"
 import scrollEffects from "./modules/scrollEffect"
@@ -8,7 +8,6 @@ import scrollEffects from "./modules/scrollEffect"
 window.onload = (() => {
   'use strict'
 
-  new Modal();
   new Header();
   new MobileMenu();
   new scrollEffects($('.grid__feature'), '85%');
